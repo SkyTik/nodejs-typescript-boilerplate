ee3fe0c8b9e48b7ce5ae55d365c5c0c8c01eb521
vunguyen <vunh@ahamove.com>
2023-10-24 16:30:15 +0700

    update pino config

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,11 +1,15 @@
-import pino from "pino";
+import pino, { Logger } from "pino";
 
-const logger = pino({
+const config = {
+  base: undefined,
   transport: {
     target: "pino-pretty",
     options: {
       colorize: true
     }
   }
-});
+};
+
+const logger: Logger = process.env.NODE_ENV === "development" ? pino(config) : pino({ base: undefined });
+
 export default logger;
